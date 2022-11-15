72fb99a220705cd5ef1c1a63d00bdeb593eab0ac
jhyun-k <jhkim1238@naver.com>
2022-11-15 16:02:22 +0900

    1115

--- React/024/my-app/src/App copy 2.js
+++ React/024/my-app/src/App copy 2.js
@@ -1,37 +1,37 @@
-import React, { useState } from 'react';
+import { useState } from 'react'
 
-function Resume(props) {
-  const [like, setLike] = useState(0);
-  const [message, setMessage] = useState('hello');
-  // const [a, b] = useState(0);
-  // const 내맘대로 = useState(0);
-  // 내맘대로[0], 내맘대로[1];
+function Resume({이름, 취미, 자기소개}){
+  const [like, setLike] = useState(0) 
+  // const [like, setLike] = useState(100) 
+  // const [like, setLike] = useState('hello world') 
 
-  function handleClickLike() {
-    // setLike(like + 1);
-    // console.log(like);
-    setLike(like + 1);
+  function handleClickLike(){
+    // like += 1
+    setLike(like + 1); // like = like + 1
+    // setLike(like + 2); // like = like + 2
+    // setLike(like + 'hello' + 'world'); // like = like + 'hello' + 'world'
+    console.log(like)
   }
 
-  function handleMouseOver(){
-    console.log('hello')
-    setMessage('world')
-  }
-
-  return(
-      <div>
-          <button onClick={handleClickLike}>like : {like}</button>
-          <button onMouseOver={handleMouseOver}>마우스 오버해보세요.</button>
-          <p>{message}</p>
-      </div>
+  return (
+    <section>
+      <h2>{이름}</h2>
+      <p>{취미}</p>
+      <p>{자기소개}</p>
+      <button onClick={handleClickLike}>{like}</button>
+    </section>
   )
 }
 
 function App() {
   return (
-    <div>
-      <Resume />
-    </div>
+    <main>
+      <Resume 
+        이름="이호준" 
+        취미="코딩" 
+        자기소개="안녕하세요. 제주코딩베이스캠프 이호준입니다."
+      />
+    </main>
   );
 }
 
--- React/024/my-app/src/App copy.js
+++ React/024/my-app/src/App copy.js
@@ -1,23 +1,20 @@
-import {useState} from 'react'
-
 function Resume({이름, 취미, 자기소개}){
-  const [like, setLike] = useState(1);
-  let clap = 0 
-    let arr = clap.toString().split('')
-    for(let i = 0; i<arr.length;i++){
-        if(arr[i]===3||arr[i]===6||arr[i]===9){
-          clap++
-        }
-    } 
-    clap?setLike(like+1):setLike(like)
-  function handleClickLike(){
-    setLike(like+1)
+  let like = 0
+
+  function clickLike(){
+    like += 1
+    console.log(like)
   }
 
   return (
     <section>
-      <button onClick={handleClickLike}>like : {like}</button>
-      <span>{clap ? clap : ''}</span>
+      <h2>{이름}</h2>
+      <p>{취미}</p>
+      <p>{자기소개}</p>
+      {/* 
+        여기가 왜 증가되지 않을까요? 변수가 변경이 된다고 하더라도 렌더링이 다시 일어나지 않기 때문입니다!
+      */}
+      <button onClick={clickLike}>{like}</button>
     </section>
   )
 }
